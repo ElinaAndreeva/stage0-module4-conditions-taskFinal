de2e7fa3233bcfd5812e1c424084a0985bf8ad54
ElinaAndreeva <lolaclouds@yandex.ru>
2023-04-15 12:51:28 +0400

    Completed the final task of the module

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,41 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
-    }
-}
+        if (month>=1 && month <=12 && year >0) {
+            boolean isLeapYearBool = year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+            int leap;
+            if (isLeapYearBool) {
+                leap = 1;
+            } else {
+                leap = 0;
+            }
+            if (leap == 1) {
+                switch (month) {
+                    case 1, 3, 5, 7, 8, 10, 12:
+                        System.out.println(31);
+                        break;
+                    case 2:
+                        System.out.println(29);
+                        break;
+                    case 4, 6, 9, 11:
+                        System.out.println(30);
+                        break;
+                    default:
+                        System.out.println("wrong number!");
+                } } else {
+                    switch (month) {
+                        case 1, 3, 5, 7, 8, 10, 12:
+                            System.out.println(31);
+                            break;
+                        case 2:
+                            System.out.println(28);
+                            break;
+                        case 4, 6, 9, 11:
+                            System.out.println(30);
+                            break;
+                        default:
+                            System.out.println("wrong number!");
+                    } }
+                }
+            }
+        }
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        int result = divider / divider;
+        if (result == 0) {
+            System.out.println("division by zero");
+        } else if (result * divider != dividend) {
+            System.out.println("cannot be divided completely");
+        } else if (result * divider == dividend){
+            System.out.println("can be divided completely");
+        }
     }
 }
